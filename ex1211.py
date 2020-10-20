a24ba30939382387ffae5d627c3d738dd16f3824
Lucas Santos <lucas.parkl@outlook.com>
2020-10-20 18:50:01 -0300

    Create ex1211.py

---
+++ ex1211.py
@@ -0,0 +1,25 @@
+
+#inicia o looping até que o usuario digite 0 nas quantidades de numeros
+while True:
+    f=0
+    x=int(input())
+    #recebe o numero e verifica se é igual a 0
+    if x==0:
+        break
+
+    #inicia a lista de numeros "num" e começa o loop para adição dos 
+    # números a lista
+
+    num=[]
+    for i  in range (x):
+        num.append(input())
+
+
+    #inicia a verificação se os numeros são iguais e se o primeiro é igual
+    for i in range(len(num)-1):
+        for j in range (len(num[i])):
+            if(num[i][j]==num[i+1][j] and num[i][0]==num[i+1][0]):
+                f+=1
+
+    #printa o resultado das repetições
+    print(f)
