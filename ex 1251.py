a88fe9d68a90a43694ed990249106727466c4fc6
Lucas Santos <lucas.parkl@outlook.com>
2020-10-21 17:16:24 -0300

    Add files via upload

---
+++ ex 1251.py
@@ -0,0 +1,25 @@
+#Atividade URI 1251 - Diga-me a Frequência
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1251
+
+#Inicia e da entrada na lista, depois gera um set auxiliar para remover 
+# os repetidos
+d=[]
+d=list(input())
+daux=list(set(d))
+daux.sort()
+#inicia a lista de contagem 
+c=[]
+#loop para contar as ocorrencias e colocar na string a letra em ASCII 
+# e a qtde de ocorrencias 
+for i in daux:
+    
+    c.append(str(ord(i))+" "+str(d.count(i)))
+
+
+#ordenar a partir da quantidade de ocorrencias
+print(daux)
+c.sort(key= lambda k:k[2],)
+c.sort(reverse=True)
+
+#printar na tela :)
+print(c)
