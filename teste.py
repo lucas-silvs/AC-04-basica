39faf3a04ccbd34d804c61924ad5555bd0ec7a9e
Lucas Santos <lucas.parkl@outlook.com>
2020-10-21 21:55:17 -0300

    problemas

--- teste.py
+++ teste.py
@@ -5,32 +5,27 @@
 #inicia o looping até que o usuario digite 0 nas quantidades de numeros
 while True:
     try:
+        # acumula os caracteres economizados
         f=0
-        x=int(input())
-       
 
+        x=int(input())
         #inicia a lista de numeros "num" e começa o loop para adição dos 
         # números a lista
-
+        
         num=[]
         for i  in range (x):
             num.append(input())
-
-
         #inicia a verificação se os numeros são iguais e se o primeiro é igual
-        
+      
         for i in range(len(num)-1):
-            q=0
             for j in range (len(num[i])):
-                if(num[i][j]==num[i+1][j]):
-                    q+=1
+                if(num[i][j]==num[i][j]):
+                    f+=1
                 else:
                     break
-            if(f<=q):
-                f=q
-
+                
+                
         #printa o resultado das repetições
         print(f)
     except EOFError:
         break
-
