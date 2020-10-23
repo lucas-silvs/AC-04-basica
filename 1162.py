c493855ab8e2bb30a33c8a7944ee4e3f51a52ef2
Lucas Santos <lucas.parkl@outlook.com>
2020-10-23 17:53:10 -0300

    23-10 ex

--- 1162.py
+++ 1162.py
@@ -2,8 +2,37 @@
 # https://www.urionlinejudge.com.br/judge/pt/problems/view/1162
 
 while True:
+    
     q=int(input())
-    for in range(q):
+    
+    for j in range(q):
+        swamp=0
+        correto=0
         n=int(input())
         vagoes=input().split()
-        
+
+
+        while correto<len(vagoes):
+            for i in range(len(vagoes)-1):
+                
+                aux=[]
+                
+                aux.append(vagoes[i])
+                aux.append(vagoes[i+1])
+                
+                if(aux[0]>aux[1]):
+                    a=aux[0]
+                    aux[0]=aux[1]
+                    aux[1]=a
+                    vagoes[i]=aux[0]
+                    vagoes[i+1]=aux[1]
+                    swamp+=1
+            
+
+            for i in range(len(vagoes)-1):
+                if(vagoes[i]<vagoes[i+1]):
+                    correto+=1
+
+        print(f'Optimal train swapping takes {swamp} swaps.')
+
+    break
--- ex 1251.py
+++ ex 1251.py
@@ -5,38 +5,30 @@ teste=0
 final=[]
 ocorrencias=0
 while True:
-    
-   
-   
-    
-       
-
-        #Inicia e da entrada na lista, depois gera um set auxiliar para remover 
-    # os repetidos
-    d=[]
-    d=list(input())
-    daux=list(set(d))
-    daux.sort()
-    #inicia a lista de contagem 
-    c=[]
-    #loop para contar as ocorrencias e colocar na como uma lista a letra 
-    # em ASCII e a qtde de ocorrencias 
-    for i in daux:
-        
-        c.append([str(ord(i)),str(d.count(i))])
+    try:
+ 
+            #Inicia e da entrada na lista, depois gera um set auxiliar para remover 
+        # os repetidos
+        d=[]
+        d=list(input())
+        daux=list(set(d))
+        daux.sort()
+        #inicia a lista de contagem 
+        c=[]
+        #loop para contar as ocorrencias e colocar na como uma lista a letra 
+        # em ASCII e a qtde de ocorrencias 
+        for i in daux:
+            
+            c.append([str(ord(i)),str(d.count(i))])
 
 
-    #ordenar a partir da quantidade de ocorrencias
+        #ordenar a partir da quantidade de ocorrencias
 
-    c.sort(key= lambda k:k[1])
-    final.append(c)
+        c.sort(key= lambda k:k[1])
+        final.append(c)
+    except EOFError:
 
-
-    #printar na tela :)
-    
-    teste+=1
-    if teste==2:
-        
+        #printar na tela :
         for i in final:
             ocorrencias+=1
             aux=i
