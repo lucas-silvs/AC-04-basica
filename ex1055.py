c626e049e9d453729cd8b50e864e843ece7c4c44
Lucas Santos <lucas.parkl@outlook.com>
2020-11-05 13:25:20 -0300

    Update ex1055.py

--- ex1055.py
+++ ex1055.py
@@ -6,23 +6,25 @@ for i in range(qtde):
     lista=[]
     ln=0
     l=input().split()
-
-    aux=[int(i)for i in l]
+    ltamanho=int(l[0])
+    del(l[0])
+    aux=[int(l[i])for i in range(ltamanho)]
     ln=int(l[0])
-    del(aux[0])
     aux.sort()
     p=len(aux)//2
-    c=True
-    if(len(aux) %2==0):
+    c=False
+    if(len(aux)%2==0):
         
         while aux!=[]:
             if(c==True):
+                
                 lista.insert(0,aux[len(aux)-1])
                 lista.append(aux[0])
                 del(aux[0])
                 del(aux[len(aux)-1])
                 c=False
             else:
+                
                 lista.insert(0,aux[0])
                 lista.append(aux[len(aux)-1])
                 del(aux[0])
@@ -32,10 +34,11 @@ for i in range(qtde):
     else:
          while aux!=[]:
             if(len(aux)==1):
+                
                 if(abs(aux[0]-lista[0])>=abs(lista[len(lista)-1]-aux[0])):
-                    lista.insert(0,aux[0])
+                    lista.append(aux[0])
                 else:
-                    lista.insert(len(lista)-1,aux[0])
+                    lista.insert(len(lista),aux[0])
                 break
             if(c==True):
                 lista.insert(0,aux[len(aux)-1])
@@ -50,6 +53,7 @@ for i in range(qtde):
                 del(aux[len(aux)-1])
                 c=True
     somanumero=0
-    for i in range(len(lista)-1):
-        somanumero=somanumero+abs(lista[i]-lista[i+1])
+ 
+    for j in range(len(lista)-1):
+        somanumero=somanumero+abs(lista[j]-lista[j+1])
     print(f'Case {i+1}: {somanumero}')
